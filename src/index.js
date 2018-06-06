187b34d119b8c7670457911f2122790f652ead3d
xdemocle <xdemocle@gmail.com>
2018-06-06 16:45:34 +0200

    major refactor removing vue plugin extra code not necessary anymore

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import { Sender, Receiver } from './chromecast-sdk';
 
 export default {
   install(Vue, options) {
-    const $chromecast = {};
-
-    const VueEvents = new Utils.Events(Vue);
-
-    Sender.prototype.$events = VueEvents.$events;
-    Receiver.prototype.$events = VueEvents.$events;
+    const $chromecast = new Vue();
 
     if (Utils.checkIfChromecast()) {
       $chromecast.Receiver = new Receiver(options);
     } else {
       $chromecast.Sender = new Sender(options);
     }
-
-    $chromecast.$on = (eventName, eventHandler) => {
-      VueEvents.$events.$on(eventName, eventHandler);
-    };
 
     // Register the vue plugin
     Vue.prototype.$chromecast = $chromecast;
--- src/utils.js
+++ src/utils.js
@@ -1,43 +1,5 @@
-class Events {
-  constructor(Vue) {
-    this.$events = new Vue({
-      methods: {
-        /**
-         * Emit the given event.
-         *
-         * @param {string|object} event
-         * @param {...*} args
-         */
-        emit(event, ...args) {
-          this.$emit(event, ...args);
-        },
-
-        /**
-         * Listen for the given event.
-         *
-         * @param {string} event
-         * @param {function} callback
-         */
-        on(event, callback) {
-          this.$on(event, callback);
-        },
-
-        /**
-         * Remove one or more event listeners.
-         *
-         * @param {string} event
-         * @param {function} callback
-         */
-        off(event, callback) {
-          this.$off(event, callback);
-        }
-      }
-    });
-  }
-}
-
 function checkIfChromecast() {
   return navigator.userAgent.indexOf('CrKey') !== -1;
 }
 
-export default { Events, checkIfChromecast };
+export default { checkIfChromecast };
